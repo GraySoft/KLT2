4d313f98697efa293cf1bf1691c8cbe6c3fb1cf6
olegpetskovich <OlegPetskovich@gmail.com>
2018-08-16 11:31:54 +0300

    1. Change design 2. Set listeners

--- app/src/main/java/com/karpachoff/klt/presentation/views/activities/LoginActivity.java
+++ app/src/main/java/com/karpachoff/klt/presentation/views/activities/LoginActivity.java
@@ -1,15 +1,55 @@
 package com.karpachoff.klt.presentation.views.activities;
 
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.karpachoff.klt.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextInputLayout layoutName;
+    TextInputEditText name;
+
+    TextInputLayout layoutPassword;
+    TextInputEditText password;
+
+    Button btnForgetPassword;
+    Button btnRegistrationLogInActivity;
+    Button btnSignInLogInActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //choose one of the options
+        layoutName = findViewById(R.id.layoutName);
+        name = layoutName.findViewById(R.id.name);
+
+        name = findViewById(R.id.email);
+
+        //choose one of the options
+        layoutPassword = findViewById(R.id.layoutPassword);
+        password = layoutPassword.findViewById(R.id.password);
+
+        password = findViewById(R.id.password);
+
+        btnForgetPassword = findViewById(R.id.btnForgetPassword);
+        btnForgetPassword.setOnClickListener(v -> {
+
+        });
+
+        btnSignInLogInActivity = findViewById(R.id.btnSignInLogInActivity);
+        btnSignInLogInActivity.setOnClickListener(v -> {
+
+        });
+
+        btnRegistrationLogInActivity = findViewById(R.id.btnRegistrationLogInActivity);
+        btnRegistrationLogInActivity.setOnClickListener(v -> {
+
+        });
     }
 }
--- app/src/main/java/com/karpachoff/klt/presentation/views/activities/RestorePasswordActivity.java
+++ app/src/main/java/com/karpachoff/klt/presentation/views/activities/RestorePasswordActivity.java
@@ -1,15 +1,41 @@
 package com.karpachoff.klt.presentation.views.activities;
 
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.karpachoff.klt.R;
 
 public class RestorePasswordActivity extends AppCompatActivity {
 
+    TextInputLayout layoutEmail;
+    TextInputEditText email;
+
+    Button btnSignInRestorePasswordActivity;
+    Button btnRegistrationRestorePasswordActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restore_password);
+
+        //one of the options
+        layoutEmail = findViewById(R.id.layoutEmail);
+        email = layoutEmail.findViewById(R.id.email);
+
+        email = findViewById(R.id.email);
+
+        btnSignInRestorePasswordActivity = findViewById(R.id.btnSignInRestorePasswordActivity);
+        btnSignInRestorePasswordActivity.setOnClickListener(v -> {
+
+        });
+
+        btnRegistrationRestorePasswordActivity = findViewById(R.id.btnRegistrationRestorePasswordActivity);
+        btnRegistrationRestorePasswordActivity.setOnClickListener(v -> {
+
+        });
     }
 }
